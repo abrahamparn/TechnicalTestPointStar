db376d49f0085eb4f122b61462e8a7c644229fa0
abrahamparn <abrahamnaiborhu@gmail.com>
2025-08-04 11:36:28 +0700

    adding commentaries and cleanup

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useAuthStore } from "../store/auth"; //get token for everything
 
-const devBaseUrl = "http://localhost:3000/api/v1";
+const devBaseUrl = "http://localhost:3000/api/v1"; // development url
 const prodBaseUrl = "/api/v1";
 const baseURL = import.meta.env.DEV ? devBaseUrl : prodBaseUrl;
 
@@ -13,6 +13,7 @@ const apiClient = axios.create({
   withCredentials: true,
 });
 
+// to add bearer token on every api hit
 apiClient.interceptors.request.use(
   (config) => {
     const { accessToken } = useAuthStore.getState();
